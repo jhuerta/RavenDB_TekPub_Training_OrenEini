700bedacc9d5589b236a45d9bee2a4b976fac8a8
Juan Huerta <juan.huerta@gmail.com>
2012-10-17 13:53:28 +0800

    Initial Commit

---
+++ RavenDB/RavenDB/Controllers/NotificationController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Web.Mvc;
+using NLog;
+using NLog.Layouts;
+using NLog.Targets;
+using Raven.Client;
+using RavenDB;
+
+namespace ASP
+{
+
+    public class RavenController : Controller
+    {
+        protected IDocumentSession documentSession;
+
+        protected override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            documentSession = MvcApplication.DocumentStore.OpenSession();
+        }
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            using(documentSession)
+            {
+                if(filterContext.Exception==null)
+                {
+                    documentSession.SaveChanges();
+                }
+            }
+        }
+    }
+    public class NotificationController : RavenController
+
+    {
+        public ActionResult NewOrder(string orderId)
+        {
+        
+            documentSession.Store(
+                new 
+                    {
+                        OrderId = orderId,
+                        OrderDate = DateTime.Now
+                    });
+            return Json(new
+                            {
+                                Success = true
+                            }, JsonRequestBehavior.AllowGet);
+        }
+         
+    }
+}
---
+++ RavenDB/RavenDB/Tasks/BackgroundTasks.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using NLog;
+using Newtonsoft.Json;
+using Raven.Client;
+
+namespace RavenDB.Tasks
+{
+    public abstract class BackgroundTasks
+    {
+
+        protected IDocumentSession documentSession;
+        private Logger log = LogManager.GetCurrentClassLogger();
+
+        public abstract void Execute();
+        public virtual void Initialize()
+        {
+            documentSession = MvcApplication.DocumentStore.OpenSession();
+        }
+
+        public void Run()
+        {
+            Initialize();
+            try
+            {
+                Execute();
+                documentSession.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                
+                log.ErrorException("Error processing task" + ToString(), ex);
+            }
+        }
+
+        public override abstract string ToString();
+    }
+
+    class ProcessNewOrderTask : BackgroundTasks
+    {
+        public string OrderId { get; set; }
+
+        public override void Execute()
+        {
+            var request = WebRequest.Create("https://www.google.com?q=" + OrderId);
+
+            WebResponse webresponse = null;
+            try
+            {
+                webresponse = request.GetResponse();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            using (var response = webresponse)
+            {
+                using (var stream = response.GetResponseStream())
+                {
+                    using(var reader = new StreamReader(stream))
+                    {
+                        var jsonSerializer = new JsonSerializer();
+                        var orderMessage = jsonSerializer.Deserialize <OrderMessage> (new JsonTextReader(reader));
+                    }
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("OrderId: {0}", OrderId);
+        }
+    }
+
+    internal class OrderMessage
+    {
+        private string Id { get; set; }
+        private string Name { get; set; }
+        private string Quantity { get; set; }
+        private string Notes { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Id: {0}, Name: {1}, Quantity: {2}, Notes: {3}", Id, Name, Quantity, Notes);
+        }
+    }
+}
